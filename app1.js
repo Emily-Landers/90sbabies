d18f95ee73b6efe3aa3178134180dbd16a2192e6
Emily Landers <emily.j.landers@gmail.com>
2021-09-02 15:04:25 -0400

    added functions

--- app1.js
+++ app1.js
@@ -1,9 +1,11 @@
+function getUserName(){
 let userName = prompt('Are you a 90s baby?');
 
 if(userName == 'No'){
     document.write('Go Away!')
 } else {
     document.write('Welcome!')
+}
 } 
-userName()
+getUserName()
 
--- app3.js
+++ app3.js
@@ -1,7 +1,10 @@
+function getPerson(){
 var person = prompt("Please enter your name");
 
 if (person != null) {
   document.getElementById("demo").innerHTML =
   "Hello " + person + "! Are you feeling nostalgic today?";
 }
-person()
+}
+getPerson()
+
