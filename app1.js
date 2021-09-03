3a8fe5d8cfcd793ae913a349626b1ec75b59c26a
Emily Landers <emily.j.landers@gmail.com>
2021-09-03 14:35:59 -0400

    fixed js, all working

--- app1.js
+++ app1.js
@@ -12,8 +12,7 @@ function getPerson(){
     var person = prompt("Please enter your name");
     
     if (person != null) {
-      document.getElementById("demo").innerHTML =
-      "Hello " + person + "! Are you feeling nostalgic today?";
+      document.write("Hello " + person + "! Are you feeling nostalgic today?");
     }
 }
     
@@ -26,14 +25,15 @@ function notlying(){
     }
 // control number img add 
 function pictures90s(){
-  let pictures90 = prompt ('How many nostalgic pictures do you want to see?')
-  for(let i = 0; i <parseInt (pictures90); i++);
-
+  let pictures90 = prompt ('How many nostalgic pictures do you want to see 1-5?')
   while((pictures90 < 1) || (pictures90 > 5)){
     pictures90 = prompt ('Pick a number 1-5')
-    let url = "1.jpg";
-    document.write('<img src="' + url + '">'); 
+  }
+
+  for(let i = 0; i < pictures90; i++){
+    let url = "3.jpg";
+    document.write('<img src="' + url + '">');
   }
 }
-pictures90s()
+
     
